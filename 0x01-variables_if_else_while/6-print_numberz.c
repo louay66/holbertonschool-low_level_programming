497a0c2c14ab81114833877c24265a6edd20c154
root <louaylouay20@gmail.com>
2022-02-04 14:26:44 -0800

    task 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0 ; ch < 10; ch++)
+
+	{
+	putchar(ch + '0');
+	}
+	putchar('\n');
+	return (0);
+}
