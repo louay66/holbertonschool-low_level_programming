f2f7ad146dabe7b9eed406a57c1945738f86ad9e
root <louaylouay20@gmail.com>
2022-02-04 13:36:09 -0800

    advense

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * * main - Entry point
+ * *
+ * * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i, a, b;
+
+	i = 0;
+	while (i < 100)
+	{
+	a = i % 10;
+	b = i / 10;
+	if (k < j)
+	{
+		putchar(a + '0');
+		putchar(b + '0');
+		if (i < 89)
+		{
+			putchar(44);
+			putchar(32);
+		}
+
+	}
+	i++;
+	}
+	putchar('\n');
+	return (0);
+}
