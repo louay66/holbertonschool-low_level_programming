e8ea2b7a2b20aff13154bf9d612a6f3d02797474
root <louaylouay20@gmail.com>
2022-02-08 10:19:30 -0800

    task lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - writes the character c to stdou
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	else
+		return (0);
+	}
+}
