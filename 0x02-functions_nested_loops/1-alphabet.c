712f980cf891c5ea416b7340c2fb07fb5dc69245
root <louaylouay20@gmail.com>
2022-02-08 03:54:10 -0800

    function print alfabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_alphabet - writes the character c to stdou
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+	void print_alphabet(void)
+	{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar(\n);
+}
