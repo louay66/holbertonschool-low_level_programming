886f1e4074f999dbe53c5adbd86bfb4763e6a817
root <louaylouay20@gmail.com>
2022-02-08 13:24:56 -0800

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add- add a and b
+ * @a : added
+ * @b : added
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
