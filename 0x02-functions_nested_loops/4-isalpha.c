05253cd7f5e498b3f36146ec4a3ee5a4ba8e9fdc
root <louaylouay20@gmail.com>
2022-02-08 10:40:27 -0800

    only alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_isalpha - writes the character c to stdou
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
