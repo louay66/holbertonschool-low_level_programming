3ebefea4887d2003802786fa01ce4bafacfd7b9a
root <louaylouay20@gmail.com>
2022-02-08 04:37:04 -0800

    task one * 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_alphabet - writes the character c to stdou
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+	void print_alphabet(void)
+	{
+	int a;
+	char ch;
+
+	for (a = 0 ; a < 10 ; a++)
+	{
+	for (ch = 'a' ; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	}
+	_putchar('\n');
+}
