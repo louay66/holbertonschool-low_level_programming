769df25aafb862f2ae6c7ec282f8863dcc458cdd
root <louaylouay20@gmail.com>
2022-02-08 10:20:34 -0800

    header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,4 @@
-#ifndef holberton_header
-#define holberton_header
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-
-
-
-
-
-
-
-
-
-
-
-#endif
+int _islower(int c);
