5c841161a5dbf03fc107f0141b1732cd99c053e7
root <louaylouay20@gmail.com>
2022-02-09 14:43:24 -0800

    fibonnacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+		unsigned long  n, b = 1, c = 0;
+
+		do
+
+	{
+		n = b + c;
+		c = b;
+		b = n;
+		if (n % 2 == 0)
+		{
+		printf("%lu\n", n);
+		}
+	}
+		while (n < 2500000);
+
+	return (0);
+}
