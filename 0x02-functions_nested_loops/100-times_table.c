8495cf5022a272dd1f1e5231ec2478ee2e7842d7
root <louaylouay20@gmail.com>
2022-02-09 12:06:51 -0800

    time table smart

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,11 +12,11 @@ void print_times_table(int n)
 
 	if ( n >= 0 && n <=15)
 	{
-		_putchar('0');
-	for (a = 0 ; a < n ; a++)
+		
+	for (a = 0 ; a <= n ; a++)
 	{
 
-		for (b = 0 ; b < n ; b++)
+		for (b = 0 ; b <= n ; b++)
 		{
 			c = a * b;
 			f = c / 100;
