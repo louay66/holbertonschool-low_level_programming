8f991c0c8160e959e7cbe9ddcdde47052cfbc718
root <louaylouay20@gmail.com>
2022-02-09 13:40:40 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, n, b = 1, c = 1;
+
+	for (a = 0 ; a < 50 ; a++)
+	{
+		n = b + c
+		printf("%d, ", n);
+		c = b;
+		b = n;
+	}
+	return (0);
+}
