a9317d6a38de3b7322b04d1523c6a3efa805f2be
root <louaylouay20@gmail.com>
2022-02-10 02:30:30 -0800

    only digt

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main1.h"
+
+/**
+ * _isdigit - only digt
+ * @c: only digt
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
