99908ea22a6acf8ee1efd4644112c240f664e844
root <louaylouay20@gmail.com>
2022-02-10 12:48:46 -0800

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_diagonal -print diagonal lenth n
+ * @n: diagonal
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n > 0)
+	{
+		for (a = 0 ; a < n ; a++)
+		{
+
+			for (b = 0 ; b <= a ; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
