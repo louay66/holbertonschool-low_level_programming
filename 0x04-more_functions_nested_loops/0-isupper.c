6ad9b513f536f29b2d1f98f8116e32567d5af478
root <louaylouay20@gmail.com>
2022-02-10 01:39:19 -0800

    only uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main1.h"
+
+/**
+ * _isupper - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
