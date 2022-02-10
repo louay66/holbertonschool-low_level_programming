b77253f0a6108e7928da9724b61b09699acd23e1
root <louaylouay20@gmail.com>
2022-02-10 05:26:35 -0800

    print most number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i != 2 && i != 4)
+		_putchar('0' + i);
+	
+	}
+	_putchar ('\n');
+}
