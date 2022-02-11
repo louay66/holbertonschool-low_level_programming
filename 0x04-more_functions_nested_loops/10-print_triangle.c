070017851e03b8d6f2b5bfe7d1711eb5511f9a76
root <louaylouay20@gmail.com>
2022-02-11 13:10:07 -0800

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_triangle -print square size
+ * @size: diagonal
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_triangle(int size)
+{
+	int a, b, i;
+
+	if (size <= 0)
+	putchar('\n');
+	{
+	for (a = 0 ; a < size ; a++)
+	{
+	for (b = size - a ; b > 1; b--)
+	{
+	_putchar(' ');
+	}
+	for (i = a + b ; i >= 1 ; i--)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
+	}
+}
