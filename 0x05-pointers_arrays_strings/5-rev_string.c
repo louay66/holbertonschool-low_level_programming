1f7a23800bf249590e06ba123ba629c686b8fceb
root <louaylouay20@gmail.com>
2022-02-14 14:16:16 -0800

    revers string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * rev_string-revers string
+ * @s: evrers string
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void rev_string(char *s)
+{
+	char v;
+	int a, b, length;
+
+	for (a = 0 ; s[a] != '\0' ; a++)
+	{
+	}
+	length = a - 1;
+
+	for (b = 0 ; b / 2 ; b++)
+	{
+		v = s[b];
+		s[b] = s[length];
+		s[a--] = v;
+
+	}
+	_putchar('\n');
+}
