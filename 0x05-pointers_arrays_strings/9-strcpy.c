fb054c350379637c803ab27eed192f1e4ab2c022
root <louaylouay20@gmail.com>
2022-02-14 14:37:07 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * *_strcpy -pointer
+ * @dest: pointer
+ * @src:pointer
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a, b;
+
+	for (a = 0 ; sec[a] != '\0' ; a++)
+	{
+	}
+	for (b = 0 ; b < a ; b++)
+	{
+		dest[a] = sec[a];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
