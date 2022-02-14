675060bf1478a12d8c0aec942931fa03a006b067
root <louaylouay20@gmail.com>
2022-02-14 11:57:26 -0800

    only even

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,11 @@
 void puts2(char *str)
 {
 	int i, c;
+
 	for (i = 0 ; str[i] != '\0' ; i++)
-	{ 
+	{
 	}
-		for (c = 0 ; c <= i ; c += 2)
+		for (c = 0 ; c < i ; c += 2)
 	{
 		_putchar(str[c]);
 	}
