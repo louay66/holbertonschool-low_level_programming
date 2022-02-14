2bce125ad8913276f0d8883399094f1ae13069b2
root <louaylouay20@gmail.com>
2022-02-14 11:50:26 -0800

    only even

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *puts2 - str pointer
+ *@str:string pointer
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void puts2(char *str)
+{
+	int i, c;
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{ 
+	}
+		for (c = 0 ; c <= i ; c += 2)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
+
+}
