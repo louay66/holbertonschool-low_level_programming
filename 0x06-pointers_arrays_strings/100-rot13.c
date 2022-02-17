87089617ed0cd15d29c48fecfc1e97a20dce9d11
root <louaylouay20@gmail.com>
2022-02-17 13:03:18 -0800

    crypte rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -16,12 +16,14 @@ char *rot13(char *s)
 	for (i = 0 ; s[i] != '\0' ; i++)
 	{
 		for (j = 0 ; j < 10 ; j++)
+		{
 			if (s[i] == a[j])
 			{
 				s[i] = b[j];
 				break;
 
 			}
+		}
 	}
 	return (s);
 }
