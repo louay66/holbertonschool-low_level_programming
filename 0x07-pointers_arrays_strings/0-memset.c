986d80c5a1c0a8b8f5ee995f17e0cf8303e6d4a2
root <louaylouay20@gmail.com>
2022-02-21 11:34:53 -0800

    change vlalue of array

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ **_memset - overwrite in data stractar
+ *@s:array
+ *@b:the new value
+ *@n: numbar of repitation
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	
+	for (i = 0 ; i < n  ; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
