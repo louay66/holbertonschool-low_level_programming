1e2812310b87423813d6cd1a1f8292ccadbd602a
root <louaylouay20@gmail.com>
2022-02-21 12:42:14 -0800

    searches for the first occurrence of the character c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ **_strchr - searches for the first occurrence of the character c
+ *@s: This is the C string to be scanned.
+ *@c:This is the character to be searched in str.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
+
