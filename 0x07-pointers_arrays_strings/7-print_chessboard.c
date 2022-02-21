a31b7f14abb6af02aaffd7e13a94a83f2cc9953a
root <louaylouay20@gmail.com>
2022-02-21 13:46:07 -0800

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_chessboard - print chessbord
+ *@: tow dimensionel array
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, b;
+
+	for (i = 0 ; i < 8 ; i++)
+	{
+		for (b = 0 ; b < 8 ; b++)
+		{
+			_putchar(a[i][b]);
+		}
+		_putchar('\n');
+	}
+}
