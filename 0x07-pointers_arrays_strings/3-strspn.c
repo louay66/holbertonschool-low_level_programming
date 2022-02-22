818acccccbdb4ac37f78f7c0a2bcf526fa801d4c
root <louaylouay20@gmail.com>
2022-02-22 13:26:50 -0800

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strspn - The strspn() function returns the length of the initial substring of the string pointed to by str1 that is made up of only those character contained in the string pointed to by str2
+ *@s:string to be scanned
+ *@accept:string containing the 
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i , j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+
+
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+	}
+		if (s[i] != accept[j])
+		{
+			break;
+		}
+	}
+	return (i);
+}
