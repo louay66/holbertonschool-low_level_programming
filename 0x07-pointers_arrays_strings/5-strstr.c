7216f281d5093acd6dd01f4541240b1949cbc5a1
root <louaylouay20@gmail.com>
2022-02-22 13:55:23 -0800

    fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * *strstr - prints buffer in hexa
+ * _strstr - prints buffer in hexa
  * @haystack: the address of memory to print
  * @needle: the size of the memory to print
  *
@@ -16,7 +16,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] && haystack[i + j] == needle[j]; j++)
 		{
-			if (needle[j])
+			if (!needle[j])
 				return (&haystack[i]);
 		}
 	}
