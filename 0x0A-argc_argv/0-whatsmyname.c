0d299e1d9628d51db18e3b3d405ee83ab8fc02e5
root <louaylouay20@gmail.com>
2022-02-25 03:00:12 -0800

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *@argc: argument count
+ *@argv: argument value
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0 ; i < argc ; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
