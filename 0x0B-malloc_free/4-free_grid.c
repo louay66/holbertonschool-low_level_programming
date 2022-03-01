b50668e67f189364c4eb5d288def22ba33b24fbd
root <louaylouay20@gmail.com>
2022-03-01 14:46:48 -0800

    function that returns a pointer to a 2 dimensional array of integers

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - clear memoir
+ *@grid:grid you want to clear
+ *@height: height of grid
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; ++i) {
+		free(grid[i]);
+	}
+	free(grid);
+}
