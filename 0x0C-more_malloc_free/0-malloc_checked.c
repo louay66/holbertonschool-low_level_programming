318d6eab2afdba2359c823268434b9168406966b
root <louaylouay20@gmail.com>
2022-03-03 03:26:12 -0800

    unction that allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- **_strdup - dublicated
- *@str:string
+ **malloc_checked - malloc_checked
+ *@s:number
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -15,7 +15,7 @@ void *malloc_checked(unsigned int b)
 	s = malloc(b);
 
 		if (s == NULL)
-			exit (98);
+		exit (98);
 
 	return (s);
 }
