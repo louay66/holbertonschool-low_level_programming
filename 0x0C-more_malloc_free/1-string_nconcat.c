86907da87def24cab66b849f1d3496c4f0d53e24
root <louaylouay20@gmail.com>
2022-03-03 12:52:37 -0800

    function that allocates memory using malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *string_nconcat - connected two string 
+ *string_nconcat - connected two string
  *@s1:string
  *@s2:string
  *@n:number
@@ -12,24 +12,26 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	
-	char *a;
-
+	unsigned int x1, x2, i, j;
+	char *s;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	if (n >= strlen(s2))
-		a = (char *)malloc(sizeof (strlen(s1)) + sizeof (strlen(s2)) + 1);
-
-	a = (char *)malloc(sizeof (strlen(s1)) + sizeof (n) + 1);
-
-	if (a == NULL)
+	for (x1 = 0; s1[x1] != '\0'; x1++)
+		;
+	for (x2 = 0; s2[x2] != '\0'; x2++)
+		;
+	if (n >= x2)
+		n = x2;
+	s = malloc(sizeof(char) * (x1 + n + 1));
+	if (s == NULL)
 		return (NULL);
-
-	strncat(s1, s2, n);
-
-	return (a);
+	for (i = 0; s1[i] != '\0'; i++)
+		s[i] = s1[i];
+	for (j = x1; j < x1 + n; j++)
+		s[j] = s2[j - x1];
+	s[j] = '\0';
+	return (s);
 }
