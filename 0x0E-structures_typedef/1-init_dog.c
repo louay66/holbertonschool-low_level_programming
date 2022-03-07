92c039bc299c7b34387241b8cef5609616b482db
root <louaylouay20@gmail.com>
2022-03-07 02:02:50 -0800

    function that initialize a variable of type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *string_nconcat - connected two string
+ *@d:new strufdct
+ *@name:name of dog
+ *@owner:ownerof dog
+ *@age:age of dog
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
