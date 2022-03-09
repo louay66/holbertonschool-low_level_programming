4f924a71295a09aeb2dc77bc58b3506f742ba492
root <louaylouay20@gmail.com>
2022-03-09 08:54:50 -0800

    function that executes a function given as a parameter on each element of an array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,37 @@
+#include <string.h>
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *int_index- index
+ *@size:size of array
+ *@cmp:function
+ *@array:array
+ * Return: void
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	else
+		if (array != NULL && cmp != NULL)
+		{
+			for (i = 0 ; i < size ; i++)
+			{
+				if (cmp(array[i]) != 0)
+				{
+					break;
+				}
+			}
+			if (cmp(array[i]) == 0)
+			{
+				return (0);
+			}
+		}
+	return (i);
+}
+
