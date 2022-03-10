93806ceb7a6187bf693bb765ba8e9a69c936dd18
root <louaylouay20@gmail.com>
2022-03-10 14:08:38 -0800

    fix

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,12 +1,16 @@
-section .data
-message db "Hello, World",10
-
 section .text
-global main
+   global main
 
 main:
-mov rdi, 1
-mov rsi, msg
-mov rax, 1
-mov rdx, 13
-syscall
+   mov edx,len
+   mov ecx,msg
+   mov ebx,1
+   mov eax,4
+   int 0x80
+
+   mov eax,0
+   int 0x80
+
+section .data
+   msg: db 'Hello, World', 0xa
+   len: equ $ - msg
