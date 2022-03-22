adb47ac24c7443b7c4a08028eff566f89893967d
root <louaylouay20@gmail.com>
2022-03-22 10:22:23 -0700

    get Nth

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * get_nodeint_at_index-get the value equal index
+ *@head:node
+ *@index:index
+ * Return: void
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *inode = head;
+	unsigned int count = 0;
+
+	if (head == NULL)
+		return (NULL);
+	while (head != NULL)
+	{
+		if (count == index)
+		{
+			inode->n = head->n;
+		}
+		count++;
+		head = head->next;
+	}
+	return (inode);
+}
+
+
