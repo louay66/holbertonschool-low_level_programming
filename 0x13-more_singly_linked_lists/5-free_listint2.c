a25205c089542151a50d552addd3ac6c6d352a82
root <louaylouay20@gmail.com>
2022-03-22 05:07:37 -0700

    fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,21 +3,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *free_listint2-free strctur
- *@head:node
- * Return: void
- */
+ *  *free_listint2-free strctur
+ *   *@head:node
+ *    * Return: void
+ *     */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
-
-	if (*head == NULL)
+	listint_t *nnode, *cnode;
+	
+	cnode = *head;
+	if (cnode == NULL)
 		return;
 
-	while (*head)
+	while (cnode)
 	{
-		free(*head);
-		*head = temp->next;
+		nnode = cnode->next;
+		free(cnode);
+		cnode = nnode;
 	}
 
 	*head = NULL;
