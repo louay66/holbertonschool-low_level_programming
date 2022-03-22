5c19139597ce1f7e972aa19efb767a7f7f4698e6
root <louaylouay20@gmail.com>
2022-03-22 14:51:50 -0700

    insert in node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	while (count != idx)
 	{
+
+		if (ptr == NULL || ptr->next == NULL)
+			return (NULL);
 		ptr = ptr->next;
 		count++;
 	}
