2886d839bd50671fd26f9da43e844cae33bde79c
root <louaylouay20@gmail.com>
2022-03-24 13:12:49 -0700

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ *get_bit-get bit
+ *@n:pointer
+ *@index:index
+ * Return: On success 1.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+	return (1);
+}
