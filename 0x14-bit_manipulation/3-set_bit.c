a8a1435f3d86ac6ef89a86552bb2dda642e90281
root <louaylouay20@gmail.com>
2022-03-24 14:28:50 -0700

    set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ *set_bit-set bit
+ *@n:pointer
+ *@index:index
+ * Return: On success 1.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+
+	*n |= (1 << index);
+
+	return (1);
+
+}
