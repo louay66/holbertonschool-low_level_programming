3b860aab990e81507b4911fc015de89e5fdf181f
louay66 <louaylouay20@gmail.com>
2022-05-27 14:32:21 -0700

    kk

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,29 +7,27 @@
  * @key: key of hash
  * Return: value associated with the eleme or NULL if key couldn’t be found
  */
-	char *hash_table_get(const hash_table_t *ht, const char *key)
-{char *hash_table_get(const hash_table_t *ht, const char *key)
-	{
-		unsigned long int index;
-		hash_node_t *tmp;
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *tmp;
 
-		if (!ht)
-			return (NULL);
-
-		if (!key || *key == '\0')
-			return (NULL);
-
-		index = key_index((unsigned char *)key, ht->size);
-
-		tmp = ht->array[k_index];
-
-		while (tmp)
-		{
-			if (strcmp(tmp->key, key) == 0)
-				return (tmp->value);
-			tmp = tmp->next;
-		}
-
+	if (!ht)
 		return (NULL);
+
+	if (!key || *key == '\0')
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	tmp = ht->array[k_index];
+
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
 	}
+
+	return (NULL);
 }
