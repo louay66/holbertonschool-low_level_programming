67d21c49c6faffdcc4c8661c1997482d463cd51a
louay66 <louaylouay20@gmail.com>
2022-05-27 10:58:04 -0700

    hh

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create -create hash table via the size 
+ * @size: length og hash table
+ *
+ * Description:hash table
+ * 
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashT;
+	if (size  == 0)
+		return NULL;
+	hashT = malloc(sizeof(hash_table_t));
+	if (!hashT)
+		return NULL;
+
+	hashT->size = size;
+	hashT->array = calloc((size_t)hashT->size, sizeof(hash_node_t *));
+	if (!hashT->array)
+		return NULL;
+
+	return (hashT);
+}
+
