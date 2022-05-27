e883a386454a26bc42d69d76fa32533e22f79194
louay66 <louaylouay20@gmail.com>
2022-05-27 14:33:37 -0700

    kk

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -20,7 +20,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	index = key_index((unsigned char *)key, ht->size);
 
-	tmp = ht->array[k_index];
+	tmp = ht->array[index];
 
 	while (tmp)
 	{
