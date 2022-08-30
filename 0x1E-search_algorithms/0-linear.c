d15d7916c950fbe735d96897d1b1bfede5b747e0
louay66 <louaylouay20@gmail.com>
2022-08-30 01:19:48 +0100

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,13 +4,15 @@
  * @array: array i want to search inside it
  * @size: size of array
  * @value: value i search it
+ * Return: if value out of range return -1
+ * else return index
  */
 
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array)
+	if (array && size != 0)
 	{
 		for (i = 0; i < size; i++)
 		{
